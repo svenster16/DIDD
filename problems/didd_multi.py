3a1bf5cd359405133b1401cdfef3d2185f5baad9
Sven <svenster16@gmail.com>
2019-07-29 11:37:05 -0600

    updated problems.py

--- problems/didd_multi.py
+++ problems/didd_multi.py
@@ -44,14 +44,24 @@ class Lm1bSocialMediaDepression(multi_problem_v2.MultiProblemV2):
   """LM1b and Depression mixed problem class for multitask learning."""
 
   def __init__(self, was_reversed=False, was_copy=False):
-    problemos = [lm1b.LanguagemodelLm1b32k(),
-                problems.TwitterDepression(),
-                 reddit.RedditDepression()]
-    schedule = 'step @0 1.0 0.0 0.0 @10000 1.0 0.0 0.0'
-
+    problemos = []
+    rates = []
+    for rate, also_reverse, cls in self.problems_and_rates:
+        for r in [False, True] if also_reverse else [False]:
+            problemos.append(cls(was_reversed=r))
+            rates.append(rate)
+    pmf = multi_problem_v2.epoch_rates_to_pmf(problemos, epoch_rates=rates)
+    schedule = multi_problem_v2.constant_schedule(pmf)
     super(Lm1bSocialMediaDepression, self).__init__(
         problemos, schedule, was_reversed=was_reversed, was_copy=was_copy)
-
+  @property
+  def problems_and_rates(self):
+    """Returns a list of (weight, also_reverse, problem_class) triples."""
+    return [
+        (1.0, False, lm1b.LanguagemodelLm1b32k),
+        (50.0, False, problems.TwitterDepression),
+        (10.0, False, reddit.RedditDepression),
+    ]
   @property
   def use_vocab_from_other_problem(self):
     return lm1b.LanguagemodelLm1b32k()
