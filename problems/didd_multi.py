567e5e8eecd6ed21cf5182a26d45f64870a75164
Sven <svenster16@gmail.com>
2019-07-30 10:03:50 -0600

    updated problems.py

--- problems/didd_multi.py
+++ problems/didd_multi.py
@@ -34,6 +34,7 @@ from tensor2tensor.data_generators import lm1b
 from . import problems
 from . import reddit
 from tensor2tensor.models import transformer
+from tensor2tensor.models import evolved_transformer
 from tensor2tensor.utils import registry
 from google.cloud import storage
 
@@ -89,4 +90,37 @@ class Lm1bSocialMediaDepressionV2(multi_problem_v2.MultiProblemV2):
   def vocab_type(self):
     return text_problems.VocabType.SUBWORD
 
+@registry.register_hparams
+def transformer_tall_finetune_textclass_didd():
+  """Hparams for transformer on LM for finetuning on text class problems."""
+  hparams = transformer.transformer_tall_big()
+  hparams.learning_rate_constant = 6.25e-5
+  hparams.learning_rate_schedule = ("linear_warmup*constant*linear_decay")
+  hparams.multiproblem_schedule_max_examples = 0
+  hparams.multiproblem_target_eval_only = True
+  hparams.learning_rate_warmup_steps = 50
+  # Set train steps to learning_rate_decay_steps or less
+  hparams.learning_rate_decay_steps = 25000
+  hparams.multiproblem_reweight_label_loss = True
+  hparams.multiproblem_label_weight = 0.95
+  return hparams
 
+
+@registry.register_hparams
+def evolved_transformer_big_didd():
+  """Big parameters for Evolved Transformer model on TPU."""
+  hparams = evolved_transformer.add_evolved_transformer_hparams(transformer_tall_finetune_textclass_didd())
+
+  return hparams
+@registry.register_hparams
+def evolved_transformer_big_tpu_didd():
+  """Big parameters for Evolved Transformer model on TPU."""
+  hparams = evolved_transformer_big_didd()
+  transformer.update_hparams_for_tpu(hparams)
+  hparams.max_length = 1024
+  hparams.hidden_size = 1024
+  hparams.num_heads = 16
+  hparams.filter_size = 32768  # max fitting in 16G memory is 49152, batch 2
+  hparams.batch_size = 4
+  hparams.multiproblem_vocab_size = 2**15
+  return hparams
