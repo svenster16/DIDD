7117c24631b8ef3485bab91516930976168291c4
Sven <svenster16@gmail.com>
2019-07-31 12:19:42 -0600

    updated problems.py

--- problems/reddit.py
+++ problems/reddit.py
@@ -31,78 +31,46 @@ from tensor2tensor.data_generators import text_problems
 from tensor2tensor.data_generators import lm1b
 from tensor2tensor.models import transformer
 from tensor2tensor.utils import registry
+from tensor2tensor.utils import metrics
 from google.cloud import storage
 
 import tensorflow as tf
-
-def _original_vocab(tmp_dir):
-  """Returns a set containing the original vocabulary.
-  This is important for comparing with published results.
-  Args:
-    tmp_dir: directory containing dataset.
-  Returns:
-    a set of strings
-  """
-  vocab_url = ("http://download.tensorflow.org/models/LM_LSTM_CNN/"
-               "vocab-2016-09-10.txt")
-  vocab_filename = os.path.basename(vocab_url + ".en")
-  vocab_filepath = os.path.join(tmp_dir, vocab_filename)
-  if not os.path.exists(vocab_filepath):
-    generator_utils.maybe_download(tmp_dir, vocab_filename, vocab_url)
-  return set([
-      text_encoder.native_to_unicode(l.strip())
-      for l in tf.gfile.Open(vocab_filepath)
-  ])
-
-def _replace_oov(original_vocab, line):
-  """Replace out-of-vocab words with "UNK".
-  This maintains compatibility with published results.
-  Args:
-    original_vocab: a set of strings (The standard vocabulary for the dataset)
-    line: a unicode string - a space-delimited sequence of words.
-  Returns:
-    a unicode string - a space-delimited sequence of words.
-  """
-  return u" ".join(
-      [word if word in original_vocab else u"UNK" for word in line.split()])
 
 def download_blob(tmp_dir):
     """Downloads a blob from the bucket."""
     storage_client = storage.Client()
     bucket = storage_client.get_bucket('sventestbucket')
-    corpus_filename = 'reddit_data.zip'
+
+    zip_filename = 'reddit_data_shuf.zip'
+    zip_filepath = os.path.join(tmp_dir, zip_filename)
+    zip_blob = bucket.blob('reddit_didd_data/' + zip_filename)
+
     test_filename = 'reddit_test_set.txt'
     test_filepath = os.path.join(tmp_dir,test_filename)
-    vocab_filename = 'vocab.depression_twitter.32768.subwords'
-    corpus_filepath = os.path.join(tmp_dir,corpus_filename)
-    vocab_filepath = os.path.join(tmp_dir,vocab_filename)
-    blob = bucket.blob('reddit_data/'+corpus_filename)
-    testdatablob = bucket.blob('reddit_data/' + corpus_filename)
-    blob2 = bucket.blob(vocab_filename)
-    if not os.path.exists(vocab_filepath):
-        blob2.download_to_filename(vocab_filepath)
+    test_blob = bucket.blob('reddit_didd_data/' + zip_filename)
+
     if not os.path.exists(test_filepath):
-        testdatablob.download_to_filename(test_filepath)
-    if not os.path.exists(corpus_filepath):
-        blob.download_to_filename(corpus_filepath)
+        test_blob.download_to_filename(test_filepath)
+    if not os.path.exists(zip_filepath):
+        zip_blob.download_to_filename(zip_filepath)
         import zipfile
-        with zipfile.ZipFile(corpus_filepath, 'r') as zip_ref:
+        with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
             zip_ref.extractall(tmp_dir)
 
 def _train_data_filenames(tmp_dir):
     return [
       (os.path.join(tmp_dir,
-                   "control_training.txt"), False),
+                   "control_training_shuf.txt"), False),
       (os.path.join(tmp_dir,
-                   "depression_training.txt"), True)
+                   "depression_training_shuf.txt"), True)
     ]
 
 def _dev_data_filenames(tmp_dir):
     return [
       (os.path.join(tmp_dir,
-                   "control_validation.txt"), False),
+                   "control_validation_shuf.txt"), False),
       (os.path.join(tmp_dir,
-                   "depression_validation.txt"), True)
+                   "depression_validation_shuf.txt"), True)
     ]
 def _test_data_filenames(tmp_dir):
     return [
@@ -133,20 +101,21 @@ class RedditDepression(text_problems.Text2ClassProblem):
     @property
     def is_generate_per_split(self):
         return True
-    @property
-    def vocab_filename(self):
-        return lm1b.LanguagemodelLm1b32k().vocab_filename
+    #@property
+    #def vocab_filename(self):
+        #return lm1b.LanguagemodelLm1b32k().vocab_filename
     @property
     def approx_vocab_size(self):
-        return 2 ** 15  # 32768
+        return 2 ** 16
     @property
     def num_classes(self):
         return 2
-
+    """
     @property
     def num_training_examples(self):
-        """Used when mixing problems - how many examples are in the dataset."""
+            #Used when mixing problems - how many examples are in the dataset
         return 10958022
+    """
 
     def class_labels(self, data_dir):
         del data_dir
@@ -161,16 +130,19 @@ class RedditDepression(text_problems.Text2ClassProblem):
             problem.DatasetSplit.EVAL: _dev_data_filenames(tmp_dir),
             problem.DatasetSplit.TEST: _test_data_filenames(tmp_dir),
         }
-        original_vocab = _original_vocab(tmp_dir)
         files = split_files[dataset_split]
         for filepath, label in files:
             tf.logging.info("filepath = %s", filepath)
             for line in tf.gfile.Open(filepath):
-                txt = _replace_oov(original_vocab, text_encoder.native_to_unicode(line))
                 yield {
-                    "inputs": txt,
+                    "inputs": line,
                     "label": int(label),
                 }
+
+    def eval_metrics(self):
+        return [
+            metrics.Metrics.ACC, metrics.Metrics.ROC_AUC, metrics.Metrics.SET_PRECISION, metrics.Metrics.SET_RECALL
+        ]
 @registry.register_problem
 class RedditDepressionCharacters(RedditDepression):
   """Reddit depresssion classification, character level."""
@@ -198,6 +170,65 @@ class RedditDepressionCharacters(RedditDepression):
   def global_task_id(self):
     return problem.TaskID.EN_CHR_SENT
 
+@registry.register_problem
+class RedditDepressionAgg10(RedditDepression):
+    def generate_samples(self, data_dir, tmp_dir, dataset_split):
+
+        POST_AGG_COUNT = 10 #number of posts to aggragate together
+
+        download_blob(tmp_dir)
+        # Generate examples
+        split_files = {
+            problem.DatasetSplit.TRAIN: _train_data_filenames(tmp_dir),
+            problem.DatasetSplit.EVAL: _dev_data_filenames(tmp_dir),
+            problem.DatasetSplit.TEST: _test_data_filenames(tmp_dir),
+        }
+        files = split_files[dataset_split]
+        for filepath, label in files:
+            tf.logging.info("filepath = %s", filepath)
+            count = 0
+            aggtext = ''
+            for line in tf.gfile.Open(filepath):
+                if count == POST_AGG_COUNT:
+                    yield {
+                        "inputs": aggtext,
+                        "label": int(label),
+                    }
+                    count = 0
+                    aggtext = ''
+                    continue
+                aggtext = aggtext + ' ' + line
+                count += 1
+
+@registry.register_problem
+class RedditDepressionAgg20(RedditDepression):
+    def generate_samples(self, data_dir, tmp_dir, dataset_split):
+
+        POST_AGG_COUNT = 20 #number of posts to aggragate together
+
+        download_blob(tmp_dir)
+        # Generate examples
+        split_files = {
+            problem.DatasetSplit.TRAIN: _train_data_filenames(tmp_dir),
+            problem.DatasetSplit.EVAL: _dev_data_filenames(tmp_dir),
+            problem.DatasetSplit.TEST: _test_data_filenames(tmp_dir),
+        }
+        files = split_files[dataset_split]
+        for filepath, label in files:
+            tf.logging.info("filepath = %s", filepath)
+            count = 0
+            aggtext = ''
+            for line in tf.gfile.Open(filepath):
+                if count == POST_AGG_COUNT:
+                    yield {
+                        "inputs": aggtext,
+                        "label": int(label),
+                    }
+                    count = 0
+                    aggtext = ''
+                    continue
+                aggtext = aggtext + ' ' + line
+                count += 1
 @registry.register_hparams
 def transformer_oom():
   """
